280047557455e5e504304cad2770e5300beb6294
Chris Jantzen <jantzeca@mail.uc.edu>
2019-06-02 18:53:12 -0400

    new component and route

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="signup">
+    <CreateUser/>
+  </div>
+</template>
+
+<script>
+import CreateUser from "@/components/CreateUser.vue";
+
+export default {
+  name: "signup",
+  components: {
+    CreateUser
+  }
+};
+</script>
